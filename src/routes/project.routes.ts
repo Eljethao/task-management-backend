ee516ed1630de2841997a00b079ed5d00b1e56b3
eljethao <eljethao@gmail.com>
2026-06-11 09:38:45 +0700

    feat: update project routes to include 'Developer' role for patch and delete document operations

--- src/routes/project.routes.ts
+++ src/routes/project.routes.ts
@@ -17,8 +17,8 @@ router.use(authenticate);
 router.get('/', getProjects);
 router.get('/:id', getProjectById);
 router.post('/', authorize('Admin', 'Project Manager', 'Lead Team'), uploadProjectFiles, createProject);
-router.patch('/:id', authorize('Admin', 'Project Manager', 'Lead Team'), uploadProjectFiles, updateProject);
-router.delete('/:id/documents/:docUrl', authorize('Admin', 'Project Manager', 'Lead Team'), deleteProjectDocument);
+router.patch('/:id', authorize('Admin', 'Project Manager', 'Lead Team', 'Developer'), uploadProjectFiles, updateProject);
+router.delete('/:id/documents/:docUrl', authorize('Admin', 'Project Manager', 'Lead Team', 'Developer'), deleteProjectDocument);
 router.delete('/:id', authorize('Admin', 'Project Manager', 'Lead Team'), deleteProject);
 
 export default router;
